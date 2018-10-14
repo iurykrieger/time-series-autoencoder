fb41c7d4ceaa7424479a2c78748a190fba922eb1
Iury Krieger <iurykrieger96@gmail.com>
2018-10-14 15:21:08 -0300

    reshape and RMSE

--- lstm.py
+++ lstm.py
@@ -56,9 +56,6 @@ scaled = scaler.fit_transform(values)
 # frame as supervised learning
 reframed = series_to_supervised(scaled, 1, 1)
 
-# drop columns we don't want to predict
-reframed.drop(reframed.columns[[]], axis=1, inplace=True)
-
 
 
 
@@ -84,12 +81,12 @@ print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
 
 # design network
 model = Sequential()
-model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
+model.add(LSTM(150, input_shape=(train_X.shape[1], train_X.shape[2])))
 model.add(Dense(1))
 model.compile(loss='mae', optimizer='adam')
 
 # fit network
-history = model.fit(train_X, train_y, epochs=25, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
+history = model.fit(train_X, train_y, epochs=20, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
 
 # plot history
 pyplot.plot(history.history['loss'], label='train')
@@ -98,16 +95,18 @@ pyplot.legend()
 pyplot.show()
 
 
-
-
-
 # make a prediction
 yhat = model.predict(test_X)
 
-pyplot.plot(test_y)
-pyplot.plot(yhat)
+
+# plot prediction
+pyplot.plot(test_y, label='test')
+pyplot.plot(yhat, label='predicted')
+pyplot.legend()
 pyplot.show()
 
+
+# reshape to RMSE
 test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
 
 # invert scaling for forecast
