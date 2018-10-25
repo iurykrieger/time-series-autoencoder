2b1b62e0a6a354035b3744b8782d820c082a94a6
Iury Krieger <iurykrieger96@gmail.com>
2018-10-24 22:55:18 -0300

    other apikeys and trace

--- time_series.py
+++ time_series.py
@@ -1,6 +1,7 @@
 from metrics import get_metrics_by_endpoint, parse_metrics, write_metrics_file
 from plot import plot
 from os import path
+import traceback
 
 APIKEYS = [
     "centauro-v5",
@@ -11,7 +12,13 @@ APIKEYS = [
     "autoline",
     "extra",
     "pontofrio",
-    "casasbahia"
+    "casasbahia",
+    "ns-zattini",
+    "panvel-v5",
+    "connectparts",
+    "tokstok",
+    "marabraz",
+    "pucwebstore"
 ]
 
 ENDPOINTS = [
@@ -29,14 +36,16 @@ features = []
 values = {}
 
 for apikey in APIKEYS:
+    try:
+        apikey_data_file = "metrics/{apikey}_90d.csv".format(apikey=apikey)
+        apikey_figure_file = "figures/{apikey}_90d.png".format(apikey=apikey)
 
-    apikey_data_file = "metrics/{apikey}_90d.csv".format(apikey=apikey)
-    apikey_figure_file = "figures/{apikey}_90d.png".format(apikey=apikey)
+        if not path.isfile(apikey_data_file):
+            for endpoint in ENDPOINTS:
+                parse_metrics(get_metrics_by_endpoint(apikey, endpoint, START, END), features, values)
 
-    if not path.isfile(apikey_data_file):
-        for endpoint in ENDPOINTS:
-            parse_metrics(get_metrics_by_endpoint(apikey, endpoint, START, END), features, values)
+            write_metrics_file(apikey, START, END, features, values)
 
-        write_metrics_file(apikey, START, END, features, values)
-
-    plot(apikey, apikey_data_file, apikey_figure_file)
+        plot(apikey, apikey_data_file, apikey_figure_file)
+    except Exception as e:
+        traceback.print_exc()
